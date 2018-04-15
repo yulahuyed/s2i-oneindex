404a1ffe2906ec4b8b8c6b0c97ab4d47717d93ee
yulahuyed <yulahuyed@vpsorg.top>
2018-04-15 15:11:16 +0800

    Update start.sh

--- basefs/opt/bin/start.sh
+++ basefs/opt/bin/start.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+
+sed -i "s/pgsql/$DB_TYPE/g" /opt/app-root/src/public/tt-rss/config.php
+sed -i "s/localhost/$DB_HOST/g" /opt/app-root/src/public/tt-rss/config.php
+sed -i "s/yhiblog/$DB_USER/g" /opt/app-root/src/public/tt-rss/config.php
+sed -i "s/yhiblog/$DB_NAME/g" /opt/app-root/src/public/tt-rss/config.php
+sed -i "s/yhiblog/$DB_PASS/g" /opt/app-root/src/public/tt-rss/config.php
+sed -i "s/5432/$DB_PORT/g" /opt/app-root/src/public/tt-rss/config.php
+sed -i "s/https:\/\/shui.azurewebsites.net\//$SELF_URL_PATH/g" /opt/app-root/src/public/tt-rss/config.php
+
+
+echo "*/5 * * * * php /opt/app-root/src/tt-rss/update.php --feeds --quiet" > /opt/app-root/src/public/tt-rss/crontab
+nohup /opt/app-root/src/public/tt-rss/supercronic /opt/app-root/src/public/tt-rss/crontab > /dev/null 2>&1 &
+
 # Creates the .env file needs by Laravel using
 # The environment variables set both by the system
 # and the openshift application (deploy configuration)
