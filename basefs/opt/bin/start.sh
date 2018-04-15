4dd6ca2528e1db447b9734bcc85904f8d6989b6d
yulahuyed <yulahuyed@vpsorg.top>
2018-04-15 20:56:49 +0800

    Update start.sh

--- basefs/opt/bin/start.sh
+++ basefs/opt/bin/start.sh
@@ -3,16 +3,8 @@
 if [ "${DB_USER}" ]
 then
 
-if [ "${DB_TYPE}" ]
-then
 sed -i "s/pgsql/$DB_TYPE/g" ${HOME}/public/tt-rss/config.php
-fi
-
-if [ "${DB_PORT}" ]
-then
 sed -i "s/5432/$DB_PORT/g" ${HOME}/public/tt-rss/config.php
-fi
-
 sed -i "s/127.0.0.1/$DB_HOST/g" ${HOME}/public/tt-rss/config.php
 sed -i "s/Uyhiblog/$DB_USER/g" ${HOME}/public/tt-rss/config.php
 sed -i "s/Nyhiblog/$DB_NAME/g" ${HOME}/public/tt-rss/config.php
